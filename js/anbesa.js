f257148a6bdefd409e94bd31206bf6be0eae7c18
Fireayehu Zekarias <fireayehuzekarias1989@gmail.com>
2019-05-21 21:26:05 +0300

    First Commit

---
+++ js/anbesa.js
@@ -0,0 +1,249 @@
+function myFunction() {
+    var table;
+    var text;
+    var x = document.getElementById("numb").value;
+    if (x == 1) {
+        text = "<td>1</td><td>Megenaga</td><td>Kotebe</td><td>Kara</td><td>1.60 </td><td>8.1</td>";
+    } else if (x == 2) {
+        text = "<td>2</td><td>Merkato</td><td>mekanisa</td><td>Kore</td><td>2.25 </td><td>11.1</td>";
+    } else if (x == 3) {
+        text = "<td>3</td><td>Meskel adebabay</td><td>Total</td><td>Ayer Tena</td><td>2.25 </td><td>10.8</td>";
+    } else if (x == 4) {
+        text = "<td>4</td><td>merkato</td><td>saris</td><td>kality</td><td>3.75 </td><td>19.4</td>";
+    } else if (x == 5) {
+        text = "<td>5</td><td>minilik Adebabay</td><td>mekanisa</td><td>Kore</td><td>3.00 </td><td>12.7</td>";
+    } else if (x == 6) {
+        text = "<td>6</td><td>Semen Gebeya</td><td>Mexico</td><td>Kera</td><td>2.25 </td><td>9.9</td>";
+    } else if (x == 7) {
+        text = "<td>7</td><td></td><td>legetafo</td><td>beke</td><td>9.00 </td><td>44.0</td>";
+    } else if (x == 8) {
+        text = "<td>8</td><td>merkato</td><td>-</td><td>kechene</td><td>1.60 </td><td>9.4</td>";
+    } else if (x == 9){
+        text = "<td>9</td>piassia<td></td><td>22</td><td></td><td>Bole high school</td><td>2.25 </td><td>10.5</td>";
+    } else if (x == 10) {
+        text = "<td>10</td>piassia<td></td><td>lamberet</td><td></td><td>3.00 </td><td>12.7</td>";
+    } else if (x == 11) {
+        text = "<td>11</td>Kolfe<td></td><td>6 kilo</td><td>minilik</td><td>2.25 </td><td>10.0</td>";
+    } else if (x == 12) {
+        text = "<td>12</td><td>merkato</td><td>6 kilo</td><td>Ferensia Gurara</td><td>2.25 </td><td>9.9</td>";
+    } else if (x == 13) {
+        text = "<td>13</td><td>merkato</td><td>6 kilo</td><td>Bela</td><td>2.25 </td><td>9.9</td>";
+    } else if (x == 14) {
+        text = "<td>14</td><td>piassia</td><td>gotera</td><td>Saris abo</td><td>3.00 </td><td>12.30</td>";
+    } else if (x == 15) {
+        text = "<td>15</td><td>merkato</td><td>Abu are</td><td>Megenaga</td><td>2.25 </td><td>10.4</td>";
+    } else if (x == 16) {
+        text = "<td>1</td><td>merkato</td><td>menen</td><td>Kidane miheret</td><td>1.60 </td><td>7.9</td>";
+    } else if (x == 17){
+        text = "<td>1</td><td>merkato</td><td>shiro meda</td><td>Kuskum</td><td>2.25 </td><td>9.1</td>";
+    } else if (x == 18) {
+        text = "<td>1</td><td>merkato</td><td>mesalemia</td><td>Keranyo medan</td><td>1.60 </td><td>7.3</td>";
+    } else if (x == 19) {
+        text = "<td>1</td><td>piassia</td><td>-</td><td></td><td>Asko</td><td>2.25 </td><td>10.5</td>";
+    } else if (x == 20) {
+        text = "<td>1</td><td>merkato</td><td>Rufahel</td><td>Del ber</td><td>1.60 </td><td>8.6</td>";
+    } else if (x == 21) {
+        text = "<td>21</td><td>merkato</td><td>Lukanda</td><td>Fili Doro</td><td>1.60 </td><td>8.6</td>";
+    } else if (x == 22) {
+        text = "<td>22</td><td>Legahar</td><td>CMC</td><td>Summit</td><td>3.00 </td><td>12.3</td>";
+    } else if (x == 23) {
+        text = "<td>23</td>merkato<td></td><td>Megenaga</td><td>lamberet</td><td>3.00 </td><td>12.4</td>";
+    } else if (x == 24) {
+        text = "<td>24</td><td>merkato</td><td>AskoA</td><td>Burau</td><td>2.75 </td><td>17.4</td>";
+    } else if (x == 25){
+        text = "<td>25</td><td>Legahar</td><td>saris</td><td>Akaki</td><td>3.50 </td><td>19.0</td>";
+    } else if (x == 26) {
+        text = "<td>26</td><td>merkato</td><td>Ayer Tena</td><td>Sebeta</td><td>5.50 </td><td>25.5</td>";
+    } else if (x == 27) {
+        text = "<td>27</td><td>Legahar</td><td>Saris abo</td><td>Korki factory</td><td>2.25 </td><td>11.4</td>";
+    } else if (x == 28) {
+        text = "<td>28</td><td>merkato</td><td>Tiwan</td><td>asko Sansusi</td><td>2.25 </td><td>11.1</td>";
+    } else if (x == 29) {
+        text = "<td>29</td><td>merkato</td><td>Gotera</td><td>Addisu sefer</td><td>3.00 </td><td>12.7</td>";
+    } else if (x == 30) {
+        text = "<td>30</td><td>merkato</td><td>-</td><td>Suluta</td><td>5.50 </td><td>30.0</td>";
+    } else if (x == 31) {
+        text = "<td>31</td><td>Legahar</td><td>6 kilo</td><td>shiro medan</td><td>1.60 </td><td>7.40</td>";
+    } else if (x == 32) {
+        text = "<td>32</td><td>Legahar</td><td>Megenaga</td><td>Hana mariam</td><td>2.25 </td><td>10.60</td>";
+    } else if (x == 33){
+        text = "<td>33</td><td>4 kilo</td><td>lamberet</td><td>Kotebe Gebriel</td><td>2.25 </td><td>11.40</td>";
+    } else if (x == 34) {
+        text = "<td>34</td><td>Merkato</td><td>Kera</td><td>Gofa camp</td><td>2.25 </td><td>9.80</td>";
+    } else if (x == 35) {
+        text = "<td>35</td><td>Merkato</td><td>Cherkos</td><td>Lafto Adebabay</td><td>2.25 </td><td>9.80</td>";
+    } else if (x == 36) {
+        text = "<td>36</td><td>Legahar</td><td>Ayer Tena</td><td>Kara Kore</td><td>2.25 </td><td>11.7</td>";
+    } else if (x == 37) {
+        text = "<td>37</td><td>Meskel Adebabay</td><td>Tor Hailoch</td><td>Keranyo medan</td><td>2.25 </td><td>12.0</td>";
+    } else if (x == 38) {
+        text = "<td>38</td><td>6 kilo</td><td>Giorgis</td><td>Gofa camp</td><td>2.25 </td><td>12.0</td>";
+    } else if (x == 39) {
+        text = "<td>39</td><td>Merkato</td><td>Kazanchis</td><td>Bole medhanialem</td><td>2.25 </td><td>9.60</td>";
+    } else if (x == 40) {
+        text = "<td>40</td><td>Merkato</td><td>Kotebe</td><td>Kara Alo</td><td>3.00 </td><td>17.9</td>";
+    } else if (x == 41) {
+        text = "<td>41</td><td>Merkato</td><td>4 kilo</td><td>Eyesus</td><td>2.25 </td><td>8.50</td>";
+    } else if (x == 42) {
+        text = "<td>42</td><td>Legahar</td><td>Bole</td><td>Megenaga</td><td>2.25 </td><td>9.80</td>";
+    } else if (x == 43) {
+        text = "<td>43</td><td>Merkato</td><td>Burau</td><td>menagesha</td><td>5.50 </td><td>30.20</td>";
+    } else if (x == 44) {
+        text = "<td>44</td><td>Merkato</td><td>Kotebe</td><td>Legedadi</td><td>5.50 </td><td>30.40</td>";
+    } else if (x == 45) {
+        text = "<td>45</td><td>Legahar</td><td>Meskel Adebabay</td><td>Del ber</td><td>1.60 </td><td>8.60</td>";
+    } else if (x == 46) {
+        text = "<td>46</td><td>Merkato</td><td>Kazanchis</td><td>Gergi</td><td>2.25 </td><td>11.20</td>";
+    } else if (x == 47) {
+        text = "<td>47</td><td>Merkato Paster</td><td>Shegole</td><td>Negew fere</td><td>1.60 </td><td>6.10</td>";
+    } else if (x == 48) {
+        text = "<td>48</td><td>piassia</td><td>Bole</td><td>Gergi Megenteya</td><td>2.25 </td><td><td>10.90</td>";
+    } else if (x == 49){
+        text = "<td>49</td><td>Megenaga</td><td>CMC</td><td>Meri</td><td>1.60 </td><td>8.10</td>";
+    } else if (x == 50) {
+        text = "<td>50</td><td>Megenaga</td><td>Tor Hailoch</td><td>Total</td><td>3.00 </td><td>12.10</td>";
+    } else if (x == 51) {
+        text = "<td>51</td><td>Merkato</td><td>Zenebe werk</td><td>Wera Hotel</td><td>2.25 </td><td>10.10</td>";
+    } else if (x == 52) {
+        text = "<td>52</td><td>Merkato</td><td>Bole</td><td>Gergi</td><td>3.00</td><td>14.10</td>";
+    } else if (x == 53) {
+        text = "<td>53</td><td>shiro Meda</td><td>Estifanos</td><td>Bole Michael</td><td>2.25 </td><td>11.50</td>";
+    } else if (x == 54) {
+        text = "<td>54</td><td>Legahar</td><td>Gofa mebrat hile</td><td>Lafto</td><td>2.25 </td><td>9.50</td>";
+    } else if (x == 55) {
+        text = "<td>55</td><td>Legahar</td><td>4 kilo</td><td>Ferensia Kela</td><td>2.25 </td><td>9.50</td>";
+    } else if (x == 56) {
+        text = "<td>56</td><td>shiro Meda</td><td>Gotera</td><td>Saris abo</td><td>2.25 </td><td>14.20</td>";
+    } else if (x == 57) {
+        text = "<td>57</td><td>Legahar</td><td>Megenaga</td><td>4 kilo</td><td>3.00 </td><td>14.20</td>";
+    } else if (x == 58) {
+        text = "<td>58</td><td>Legahar</td><td>Total</td><td>Alem Bank</td><td>2.25 </td><td>12.0</td>";
+    } else if (x == 59){
+        text = "<td>59</td><td>Meskel Adebabay</td><td>Zenebe werk</td><td>Weira Hotel</td><td>2.25 </td><td>11.25</td>";
+    } else if (x == 60) {
+        text = "<td>60</td><td>Legahar</td><td>kality</td><td>Debre Zeyit</td><td>9.00 </td><td>47.20</td>";
+    } else if (x == 61) {
+        text = "<td>61</td><td>Legahar</td><td>Megenaga</td><td>Ayat adebabay</td><td>3.00 </td><td>14.80</td>";
+    } else if (x == 62) {
+        text = "<td>62</td><td>Legahar</td><td>Ayer Tena</td><td>Sebeta</td><td>5.50 </td><td>23.80</td>";
+    } else if (x == 63) {
+        text = "<td>63</td><td>Merkato</td><td>Winget</td><td>Birchico Fabrica</td><td>2.25 </td><td>9.20</td>";
+    } else if (x == 64) {
+        text = "<td>64</td><td>6 kilo</td><td>Megenaga</td><td>Gorf Aswegaj 24 kebele</td><td>2.25 </td><td>9.50</td>";
+    } else if (x == 65) {
+        text = "<td>65</td><td>Merkato</td><td>Ayer Tena</td><td>Alem bank</td><td>2.25 </td><td>11.0</td>";
+    } else if (x == 66) {
+        text = "<td>66</td><td>Merkato</td><td>Ayer Tena</td><td>Kara Kore</td><td>2.25 </td><td>10.50</td>";
+    } else if (x == 67) {
+        text = "<td>67</td><td>Legahar</td><td>mekanisa</td><td>Gemo</td><td>2.25 </td><td>10.20</td>";
+    } else if (x == 68) {
+        text = "<td>68</td><td>Tor Hailoch</td><td>6 kilo</td><td>minilik Hotel</td><td>2.25 </td><td>10.60</td>";
+    } else if (x == 69){
+        text = "<td>69</td><td>Merkato</td><td>-</td><td>Lomi meda</td><td>2.25 </td><td>12.9</td>";
+    } else if (x == 70) {
+        text = "<td>70</td><td>Kazanchisa</td><td>Total</td><td>ayer tena</td><td>2.25 </td><td>11.0</td>";
+    } else if (x == 71) {
+        text = "<td>71</td><td>Balcha Hospital</td><td>-</td><td>Bole high school</td><td>2.25 </td><td>10.60</td>";
+    } else if (x == 72) {
+        text = "<td>72</td><td>Hana mariam</td><td>-</td><td>Saris</td><td>1.00 </td><td>12.20</td>";
+    } else if (x == 73) {
+        text = "<td>73</td><td>Legahar</td><td>Ring road</td><td>Winget</td><td>2.25 </td><td>10.20</td>";
+    } else if (x == 74) {
+        text = "<td>74</td><td>Merkato</td><td>Megenaga</td><td>CMC</td><td>3.00 </td><td>13.30</td>";
+    } else if (x == 75) {
+        text = "<td>75</td><td>6 kilo</td><td>gotera</td><td>Kara</td><td>2.25 </td><td>10.40</td>";
+    } else if (x == 76) {
+        text = "<td>76</td><td>Megenaga</td><td>Saris abo</td><td>Korki factory</td><td>3.00 </td><td>12.10</td>";
+    } else if (x == 77) {
+        text = "<td>77</td><td>Kera</td><td>Total</td><td>Ayer Tena</td><td>3.00 </td><td>13.0</td>";
+    } else if (x == 78) {
+        text = "<td>78</td><td>Megenaga</td><td>-</td><td>mebrat hile</td><td>3.00</td><td>12.40</td>";
+    } else if (x == 79){
+        text = "<td>79</td><td>4 kilo</td><td>Megenaga</td><td>Summit</td><td>3.00 </td><td>11.40</td>";
+    } else if (x == 80) {
+        text = "<td>80</td><td>Megenaga</td><td>Kazanchis</td><td>Semen Gebeya</td><td>3.00 </td><td>12.40</td>";
+    } else if (x == 81) {
+        text = "<td>81</td><td>6 killo</td><td>Piassa</td><td>Aseco</td><td>3</td><td>12.4</td>";
+    } else if (x == 82) { 
+        text= "<td>82</td><td>Balcha Hospital</td><td>Megenagna</td><td>Gereje Yerer</td><td>3</td><td>14.6</td>";
+    } else if (x == 83) { 
+        text= "<td>83</td><td>6 Killo</td><td>Shola</td><td>CMc Meri</td><td>3</td><td>14.8</td>";
+    } else if (x == 84) { 
+        text= "<td>84</td><td>Legehar</td><td>Coca Cola</td><td>Mesalemiya Kolfe</td><td>2.25</td><td>9.5</td>";
+    } else if (x == 85) { 
+        text= "<td>85</td><td>Merekato</td><td>Burau</td><td>Welete</td><td>9</td><td>36</td>";
+    } else if (x == 86) { 
+        text= "<td>86</td><td>Saris Abo</td><td>Ring Road</td><td>Ayer Tena</td><td>3</td><td>13</td>";
+    } else if (x == 87) { 
+        text= "<td>87</td><td>Winget</td><td>Ring Road</td><td>Ayer Tena</td><td>2.25</td><td>10.5</td>";
+    } else if (x == 88) { 
+        text= "<td>88</td><td>Merekato</td><td>-</td><td>Chancho</td><td>9</td><td>40</td>";
+    } else if (x == 89) { 
+        text= "<td>89</td><td>Merekato</td><td>-</td><td>Sendafa</td><td>9</td><td>44</td>";
+    } else if (x == 90) { 
+        text= "<td>90</td><td>Legehar</td><td>Zenebe Werek</td><td>Weyira</td><td>2.25</td><td>10</td>";
+    } else if (x == 91) { 
+        text= "<td>91</td><td>Merekato</td><td>Sebeta</td><td>Teji</td><td>12</td><td>44</td>";
+    } else if (x == 92) { 
+        text= "<td>92</td><td>Balcha H0spita</td><td>-</td><td>Ana Mariyam</td><td>2.25</td><td>9.6</td>";
+    } else if (x == 93) { 
+        text= "<td>93</td><td>Megenagna</td><td>Saris</td><td>Bole Bulbula</td><td>3</td><td>14.4</td>";
+    } else if (x == 94) { 
+        text= "<td>94</td><td>Piassa</td><td>Winget</td><td>Mikililand Condominium</td><td>2.25</td><td>-</td>";
+    } else if (x == 95) { 
+        text= "<td>95</td><td>Merekato</td><td>-</td><td>Addis Alem</td><td>9</td><td>44</td>";
+    } else if (x == 96) { 
+        text= "<td>96</td><td>Megenagna</td><td>-</td><td>Yere</td><td>1.6</td><td>-</td>";
+    } else if (x == 97) { 
+        text= "<td>97</td><td>Megenagna</td><td>-</td><td>Legetafo</td><td>3</td><td>-</td>";
+    } else if (x == 98) { 
+        text= "<td>98</td><td>Saris Abo</td><td>-</td><td>Dukem</td><td>5.5</td><td>-</td>";
+    } else if (x == 99) { 
+        text= "<td>99</td><td>Alem Gena</td><td>-</td><td>Ayer Tena</td><td>1.60</td><td>-</td>";
+    } else if (x == 100) { 
+        text= "<td>100</td><td>Merekato</td><td>Mekainsa</td><td>Jemo</td><td>3</td><td>-</td>";
+    } else if (x == 101) { 
+        text= "<td>101</td><td>Megenagna</td><td>Yeka Ayat</td><td>Kutr 1 Condomniuim</td><td>2,25</td><td>-</td>";
+    } else if (x == 102) { 
+        text= "<td>102</td><td>Legehar</td><td>1 kuter Menged</td><td>Kara Alo</td><td>3</td><td>-</td>";
+    } else if (x == 103) { 
+        text= "<td>103</td><td>Piassa</td><td>Mexico</td><td>Jemo</td><td>3</td><td>-</td>";
+    } else if (x == 104) { 
+        text= "<td>104</td><td>saris Abo</td><td>-</td><td>Werku Sefer</td><td>1.60</td><td>-</td>";
+    } else if (x == 105) { 
+        text= "<td>105</td><td>Tore Hayiloch</td><td>Holland Embassy</td><td>Info Meda</td><td>2,25</td><td>-</td>";
+    } else if (x == 106) { 
+        text= "<td>106</td><td>Guredshola</td><td>CMC</td><td>Goro</td><td>2.25</td><td>-</td>";
+    } else if (x == 107) { 
+        text= "<td>107</td><td>Saris Abo</td><td>Kalit</td><td>Akaki Korekoro</td><td>2.25</td><td>-</td>";
+    } else if (x == 108) { 
+        text= "<td>108</td><td>Minilik Adebabay</td><td>18 Mazoriya</td><td>Asko adisu Sefer</td><td>3</td><td>-</td>";
+    } else if (x == 109) { 
+        text= "<td>109</td><td>Saris Abo</td><td>Kaliti</td><td>Tulu Dimitu</td><td>2.25</td><td>-</td>";
+    } else if (x == 110) { 
+        text= "<td>110</td><td>Akaki Kaliti</td><td>Meskel Adebabayi</td><td>6 Killo</td><td>5.5</td><td>-</td>";
+    } else if (x == 111) { 
+        text= "<td>111</td><td>Piassa</td><td>Burayu</td><td>Dire Soliya</td><td>2.25</td><td>-</td>";
+    } else if (x == 112) { 
+        text= "<td>112</td><td>Ayer Tena</td><td>Saris</td><td>Megenagna Adebabayi</td><td>2.25</td><td>-</td>";
+    } else if (x == 113) { 
+        text= "<td>113</td><td>Kaliti Total</td><td>Akaki Deldeyi</td><td>Kilinto</td><td>2.25</td><td>-</td>";
+    } else if (x == 114) { 
+        text= "<td>114</td><td>Saris Abo</td><td>-</td><td>Gelan Condominium</td><td>2.25</td><td>-</td>";
+    } else if (x == 115) { 
+        text= "<td>115</td><td>Mexico</td><td>Lebu</td><td>Muzika Bet</td><td>2.25</td><td>-</td>";
+    } else if (x == 116) { 
+        text= "<td>116</td><td>Mikael Adebabayi</td><td>Jemo 2</td><td>NOC</td><td>1</td><td>-</td>";
+    } else if (x == 117) { 
+        text= "<td>117</td><td>Hile Garment</td><td>-</td><td>Mexico</td><td>2,25</td><td>-</td>";
+    } else if (x == 118) { 
+        text= "<td>118</td><td>Merekato</td><td>Ahewa Meda</td><td>Tatek</td><td>3</td><td>-</td>";
+    } else if (x == 119) { 
+        text= "<td>119</td><td>Megenagna</td><td>-</td><td>Condominium</td><td>1</td><td>-</td>";
+    } else {
+        text = "THERE IS NO SUCH A BUS NUMBER!!";
+    }
+table="<th>Bus Number</th><th>Starting</th><th>Via</th><th>Destination</th><th>Tariff(birr)</th><th>Distance(Km)</th>"
+document.getElementById("demo1").innerHTML = table;
+document.getElementById("demo").innerHTML = text;}
+
---
+++ js/contact.js
@@ -0,0 +1,11 @@
+function myFunction(){
+	var uname=document.getElementById('fname').value;
+	var email=document.getElementById('email').value;
+	var message=document.getElementById('message').value;
+	if(unmae=null || uname=""){
+		document.getElementById("invalid1").innerHTML="Invalid input";}
+	if(email=null || email=""){
+		document.getElementById("invalid2").innerHTML="Invalid input";}
+	if(message=null || message=""){
+		document.getElementById("invalid3").innerHTML="Invalid input";}
+	}
